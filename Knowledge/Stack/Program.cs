37108d8ccbca23157f01beedf40dad4c61af19d2
hoainam2k1 <77981347+hoainam2k1@users.noreply.github.com>
2024-07-06 02:29:37 +0700

    stack
    stack

---
+++ Knowledge/Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿// LIFO (Last in First out) - Sử dụng để thực hiện các thao tác hoàn tác, xử lý biểu thức
+using System;
+using System.Collections;
+public class SamplesStack
+{
+    public static void Main()
+    {
+        Stack stack = new Stack();
+        stack.Push("Hello");
+        stack.Push("World");
+        stack.Push("!");
+
+        // Display the properties and values of the stack
+        Console.WriteLine("My stack");
+        Console.WriteLine("\tCount: {0}", stack.Count);
+        Console.WriteLine("\tValue:");
+        PrintValues(stack);
+    }
+    public static void PrintValues(IEnumerable myCollection)
+    {
+        foreach (Object obj in myCollection)
+            Console.Write("    {0}", obj);
+        Console.WriteLine();
+    }
+}
