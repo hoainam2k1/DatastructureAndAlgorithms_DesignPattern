c075d3ab13f928ece37130c3631bc2e0c1042eaf
hoainam2k1 <77981347+hoainam2k1@users.noreply.github.com>
2024-07-06 02:33:35 +0700

    queue
    queue

---
+++ Knowledge/Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿// FIFO (First in - First out) - Sử dụng trong quản lý tác vụ, hàng đợi in
+using System;
+using System.Collections;
+public class SampleQueue
+{
+    public static void Main()
+    {
+        Queue myQ = new Queue();
+        myQ.Enqueue("Hello");
+        myQ.Enqueue("World");
+        myQ.Enqueue("!");
+
+        // Displays the properties and values of the Queue.
+        Console.WriteLine("myQ");
+        Console.WriteLine("\tCount:    {0}", myQ.Count);
+        Console.Write("\tValues:");
+        PrintValues(myQ);
+    }
+    public static void PrintValues(IEnumerable myCollection)
+    {
+        foreach (Object obj in myCollection)
+            Console.Write("    {0}", obj);
+        Console.WriteLine();
+    }
+}
