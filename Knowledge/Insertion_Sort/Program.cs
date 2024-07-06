4011ff264c95067978756123dd66b8d4571f8f39
hoainam2k1 <77981347+hoainam2k1@users.noreply.github.com>
2024-07-06 15:10:57 +0700

    insertion sort
    insertion sort

---
+++ Knowledge/Insertion_Sort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class SampleInsertionSort
+{
+    static void Main()
+    {
+        int[] data = new int[10];
+        Random rand = new Random();
+        int val = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = rand.Next(20, 90);
+        }
+
+        foreach (int i in data)
+        {
+            Console.WriteLine(i + " ");
+        }
+        Console.WriteLine("\n\n");
+
+        for (int i = 1; i < data.Length - 1; i++)
+        {
+            val = data[i];
+            for (int j = i - 1; j >= 0; )
+            {
+                if (val < data[j])
+                {
+                    data[j + 1] = data[j];
+                    j--;
+                    data[j + 1] = val;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        foreach (int i in data)
+        {
+            Console.WriteLine(i + " ");
+        }
+    }
+}
